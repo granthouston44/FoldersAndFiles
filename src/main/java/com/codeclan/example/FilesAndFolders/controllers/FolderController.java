3caf3aa66fb5915e40758662abea921d5ddf6209
Grant Houston <grant.houston44@hotmail.com>
2020-02-11 18:47:08 +0000

    controllers

--- src/main/java/com/codeclan/example/FilesAndFolders/controllers/FolderController.java
+++ src/main/java/com/codeclan/example/FilesAndFolders/controllers/FolderController.java
@@ -1,4 +1,41 @@
 package com.codeclan.example.FilesAndFolders.controllers;
 
+import com.codeclan.example.FilesAndFolders.models.Folder;
+import com.codeclan.example.FilesAndFolders.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class FolderController {
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders(){
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity<Folder> getFolder(@PathVariable Long id){
+        return new ResponseEntity(folderRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/folders")
+    public ResponseEntity<Folder> createFolder(@RequestBody Folder folder){
+        folderRepository.save(folder);
+        return new ResponseEntity<>(folder, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/folder/{id}")
+    public ResponseEntity<Folder> updateFolder(@RequestBody Folder folder, @PathVariable Long id){
+        folderRepository.save(folder);
+        return new ResponseEntity<>(folder, HttpStatus.CREATED);
+    }
+
+
 }
--- src/main/java/com/codeclan/example/FilesAndFolders/controllers/UserController.java
+++ src/main/java/com/codeclan/example/FilesAndFolders/controllers/UserController.java
@@ -1,4 +1,40 @@
 package com.codeclan.example.FilesAndFolders.controllers;
 
+import com.codeclan.example.FilesAndFolders.models.User;
+import com.codeclan.example.FilesAndFolders.repositories.UserRepository;
+import org.hibernate.validator.constraints.pl.REGON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        return new ResponseEntity(userRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/users/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id){
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 }
